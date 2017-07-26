c39edbd40d9072095535d6bf2f08e3f359c93fed
陈璋 <chenzhang1440@stnts.com>
2017-07-26 10:34:25 +0800

    提交composer 文件

---
+++ test_packagelist.php
@@ -0,0 +1,11 @@
+{
+    "name": "open520yin/test_packagelist",
+    "description": "open",
+    "authors": [
+        {
+            "name": "open520yin",
+            "email": "35026155@qq.com"
+        }
+    ],
+    "require": {}
+}
